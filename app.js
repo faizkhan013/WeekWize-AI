6589fd1e573453a84a208635c5574975342bc9b9
faizkhan013 <faiz.khan6718@gmail.com>
2025-08-11 16:30:11 +0400

    updates

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+import { Analytics } from "@vercel/analytics/next";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
+
+export default MyApp;
 let tasks = [];
 let currentTaskId = null;
 let stats = {
